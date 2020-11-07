9150f632c31ebd29df630bec1fd3aeff29b6cbac
ju1ce <likozar.janus@gmail.com>
2020-11-07 23:30:47 +0100

    Update Program.cs

--- HmdPipeExample/HmdPipeExample/HmdPipeExample/Program.cs
+++ HmdPipeExample/HmdPipeExample/HmdPipeExample/Program.cs
@@ -7,7 +7,7 @@ namespace HmdPipeExample
     {
         static void Main(string[] args)
         {
-            /
+            
             Console.WriteLine("Hello World!");
             NamedPipeServerStream namedPipeServer = new NamedPipeServerStream("HMDPipe");
             
